58a484f375a202b67e2999b470361fa52b6ee0ee
Lwq <943904454@qq.com>
2019-04-22 17:09:12 +0800

    使用cookie保存用户登录信息，登录改为put

--- src/main/java/com/lwq/controller/LoginController.java
+++ src/main/java/com/lwq/controller/LoginController.java
@@ -4,6 +4,8 @@ import com.lwq.common.Const;
 import com.lwq.common.ServerResponse;
 import com.lwq.model.SysUser;
 import com.lwq.service.ILoginAndAclControlService;
+import com.lwq.util.JsonMapper;
+import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.net.HttpCookie;
 
 /**
  * @Author: Lwq
@@ -29,15 +35,18 @@ public class LoginController {
      * 用户登录
      * @param username
      * @param password
-     * @param session
+     * @param httpServletResponse
      * @return
      */
-    @RequestMapping(value = "/login.json",method = RequestMethod.POST)
+    @RequestMapping(value = "/login.json")
     @ResponseBody
-    public ServerResponse<SysUser> login(String username, String password, HttpSession session){
+    public ServerResponse<SysUser> login(String username, String password, HttpServletResponse httpServletResponse){
         ServerResponse<SysUser> response = iLoginAndAclControlService.login(username, password);
         if(response.isSuccess()){
-            session.setAttribute(Const.CURRENT_USER,response.getData());
+            Cookie cookie = new Cookie(Const.CURRENT_USER, JsonMapper.object2String(response.getData()));
+            cookie.setMaxAge(60*60);//这里设置设置有效时间，单位的秒，我这里是一小时
+            cookie.setPath("/");//这里是之根目录下所有的目录都可以共享Cookie
+            httpServletResponse.addCookie(cookie);
         }
         return response;
     }
--- src/main/java/com/lwq/filter/LoginFilter.java
+++ src/main/java/com/lwq/filter/LoginFilter.java
@@ -3,14 +3,18 @@ package com.lwq.filter;
 import com.lwq.common.Const;
 import com.lwq.common.RequestHolder;
 import com.lwq.common.ServerResponse;
+import com.lwq.model.SysDept;
 import com.lwq.model.SysUser;
 import com.lwq.util.JsonMapper;
+import org.codehaus.jackson.type.TypeReference;
 
 import javax.servlet.*;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLDecoder;
 
 /**
  * @Author: Lwq
@@ -29,7 +33,16 @@ public class LoginFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
 
-        SysUser sysUser = (SysUser) req.getSession().getAttribute(Const.CURRENT_USER);
+        SysUser sysUser = null;
+        Cookie[] cookies = req.getCookies();//这里是取出Cookie
+        if ((cookies!=null)){//判断Cookie是否为空
+            for (Cookie cookie : cookies){//遍历Cookie判断有没有对应的name
+                if (cookie.getName().equals(Const.CURRENT_USER)){
+                    sysUser = JsonMapper.string2Obj(cookie.getValue(),new TypeReference<SysUser>() {
+                    });
+                }
+            }
+        }
 
         if(sysUser==null){
             resp.setCharacterEncoding("UTF-8");
